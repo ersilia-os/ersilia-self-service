d7eabcef7ba3db968acbcf2f39fc0974c68e9269
Jenna Massardo <9603391+jmassardo@users.noreply.github.com>
2024-04-23 12:44:39 -0500

    Update update_model_request_template.py

--- .github/scripts/update_model_request_template.py
+++ .github/scripts/update_model_request_template.py
@@ -5,7 +5,7 @@ from ruamel.yaml import YAML
 import argparse
 
 current_directory = os.getcwd()
-print("Current working directory:" + current_directory)
+
 # Define command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('url', help='URL of the S3 bucket')
@@ -16,22 +16,20 @@ print("Attempting to fetch the list of models from: " + args.url)
 
 # Fetch JSON file from S3 bucket
 response = requests.get(args.url)
-print(response.text)
 data = response.json()
-
-# write out the data to the console
-print(data)
 
 # Load YAML file
 yaml = YAML()
 with open('.github/ISSUE_TEMPLATE/model-inference-run.yml', 'r') as file:
     yaml_data = yaml.load(file)
 
-print(yaml_data)
-
 # Update dropdown list
 dropdown = yaml_data['body'][1]  # Assuming the dropdown is the second item in the 'body' list
-dropdown['attributes']['options'] = [item['model_id'] for item in data if 'model_id' in item]  # Extract 'model_id' values
+dropdown['attributes']['options'] = ["{} - {}".format(item['Title'], item['Identifier']) for item in data if 'Identifier' in item and 'Title' in item and item['Status'] == 'Ready']
+
+# Sort the dropdown list
+dropdown['attributes']['options'] = sorted(dropdown['attributes']['options'])
+
 # Write back to the YAML file
 with open('.github/ISSUE_TEMPLATE/model-inference-run.yml', 'w') as file:
     yaml.dump(yaml_data, file)
